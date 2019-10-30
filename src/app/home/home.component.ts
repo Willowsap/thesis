920ba266b28d13665500d3c146a7c1c4a1815912
Willowsap <willowsapphire@protonmail.com>
2019-10-30 11:45:28 -0400

    initial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { TopnavComponent } from '../topnav/topnav.component';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
